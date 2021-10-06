e6e3a1df28ef7772110f4f9a16f429a060e04521
MatyasVasek <89735251+MatyasVasek@users.noreply.github.com>
2021-10-06 23:41:33 +0200

    Update component.py

--- component.py
+++ component.py
@@ -1,26 +1,5 @@
 import sympy
 
-"""
-class Component:
-    def __init__(self, name=None, type=None, node1=None, node2=None, node3=None, node4=None, sym_value=None,
-                 init_cond=None, position=None, value=None, control_voltage=None, shorted_component=None,
-                 dc_value=None, ac_value=None):
-        self.name = name
-        self.type = type
-        self.value = value
-        self.sym_value = sym_value
-        self.init_cond = init_cond
-        self.node1 = node1
-        self.node2 = node2
-        self.node3 = node3
-        self.node4 = node4
-        self.position = position
-        self.control_voltage = control_voltage
-        self.shorted_component = shorted_component
-        self.dc_value = dc_value
-        self.ac_value = ac_value
-"""
-
 
 class Component:
     def __init__(self, name=None, type=None, node1=None, node2=None, sym_value=None, value=None):
