64f4817f4fdc8a27bdd1306cd23619eb1d862fda
MatyasVasek <matyas.vasek@gmail.com>
2023-12-05 22:38:55 +0100

    Test scripts updated

--- tests/auto_test.py
+++ tests/auto_test.py
@@ -12,7 +12,7 @@ def test_analysis(analysis_type, is_symbolic=True):
 
     for filename in netlists:
         try:
-            circuit = AnalyseCircuit(utils.load_file("netlists/{}".format(filename)), analysis_type, is_symbolic)
+            circuit = AnalyseCircuit(utils.load_file("netlists/{}".format(filename)), analysis_type=analysis_type, symbolic=is_symbolic)
             print("{}[OK]: {} analysis of {} successful".format('\033[96m', analysis_type, filename))
         except:
             print("{}[FAIL]: {} analysis of {} ended with an error".format('\033[91m', analysis_type, filename))
@@ -25,12 +25,12 @@ def numeric_test(analysis_type, time=1):
         netlists.append(filename)
 
     for filename in netlists:
-        circuit = AnalyseCircuit(utils.load_file("netlists/{}".format(filename)), analysis_type, symbolic=False)
+        circuit = AnalyseCircuit(utils.load_file("netlists/{}".format(filename)), type=analysis_type, symbolic=False)
         node_voltages = circuit.node_voltages()
         #print(filename)
         for i in node_voltages:
             try:
-                value = node_voltages[i].subs(laplace.t, time)
+                value = node_voltages[i].subs(t, time)
             except:
                 pass
             value = value.evalf( )
@@ -43,20 +43,20 @@ if __name__ == "__main__":
     print("{}DC:".format('\033[93m'))
     print("{} Numeric test:".format('\033[95m'))
     test_analysis("DC", is_symbolic=False)
-    print("{} Symbolic test:".format('\033[95m'))
-    test_analysis("DC", is_symbolic=True)
+    #print("{} Symbolic test:".format('\033[95m'))
+    #test_analysis("DC", is_symbolic=True)
 
     print("{}TF:".format('\033[93m'))
     print("{} Numeric test:".format('\033[95m'))
     test_analysis("TF", is_symbolic=False)
-    print("{} Symbolic test:".format('\033[95m'))
-    test_analysis("TF", is_symbolic=True)
+    #print("{} Symbolic test:".format('\033[95m'))
+    #test_analysis("TF", is_symbolic=True)
 
     print("{}tran:".format('\033[93m'))
     print("{} Numeric test:".format('\033[95m'))
     test_analysis("tran", is_symbolic=False)
-    print("{} Symbolic test:".format('\033[95m'))
-    test_analysis("tran", is_symbolic=True)
+    #print("{} Symbolic test:".format('\033[95m'))
+    #test_analysis("tran", is_symbolic=True)
 
 
 
