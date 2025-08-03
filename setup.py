89aa46777848c64bb73581c3fc12537802e9f04f
MatyasVasek <vasekmat@fel.cvut.cz>
2025-08-03 18:20:09 +0200

    incremented version to 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="symcirc",
-    version="0.1.7",
+    version="0.1.8",
     author="Matyas Vasek",
     author_email="matyas.vasek@gmail.com",
     description="A python package for symbolic electronic circuit analysis",
