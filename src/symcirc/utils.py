b999acc3354a16aa48400f06a129cb2586190289
MatyasVasek <vasekmat@fel.cvut.cz>
2026-04-19 17:37:15 +0200

    - Plotting and printing improvements. - evalf() improved.

--- src/symcirc/utils.py
+++ src/symcirc/utils.py
@@ -1,14 +1,13 @@
 import sympy
 import random
-from sympy import expand, factor, simplify, limit, diff, solve, parse_expr
+from sympy import expand, factor, simplify, limit, diff, solve, parse_expr, factor_terms
 from sympy import oo as infinity
 from sympy import log, exp, sin, cos, tan, cot
 from sympy import I as j
 from sympy import pi
-from sympy import pprint
+from sympy import pretty #pprint
 
 from typing import Dict
-from logging import warning
 
 f = sympy.symbols("f", real=True, positive=True)
 s = sympy.symbols("s", real=False)
@@ -29,6 +28,9 @@ global_dict = {"pi": pi,
                "i": j,
                "j": j}
 
+def pprint(A):
+    sympy.pprint(A, wrap_line=False)
+
 def numer(H):
     N, _ = sympy.fraction(H)
     return N
@@ -37,9 +39,34 @@ def denom(H):
     _, D = sympy.fraction(H)
     return D
 
-def evalf(H, subs:Dict={}, precision:int=6):
+'''def evalf(H, subs:Dict={}, precision:int=6):
     subs_dict = {**subs, **global_dict}
-    return factor(H.evalf(subs=subs_dict, n=precision))
+    num, den = sympy.fraction(H)
+    H = sympy.simplify(num / den.as_leading_term(f))
+    print(H)
+    print(sympy.N(H))
+    return factor_terms(H.evalf(subs=subs_dict, n=precision))
+'''
+def normalize(H):
+    num, den = sympy.fraction(H)
+    # leading coefficient
+    try:
+        leading_coeff = sympy.LC(den)
+    except:
+        return H
+    scale = leading_coeff.as_coeff_Mul()[0]
+    H = (num / scale) / (den / scale)
+    return H
+
+def evalf(H, subs: Dict = {}, precision: int = 6, norm=True):
+    subs_dict = {**subs, **global_dict}
+    H = H.subs(subs_dict)
+    H = sympy.together(H)
+
+    H = H.evalf(precision)
+    if norm:
+        H = normalize(H)
+    return H
 
 def load_file(netlist_addr):
     """
@@ -52,6 +79,13 @@ def load_file(netlist_addr):
         netlist = f.read()
     return netlist
 
+def latex(expr: sympy.Expr, symbol_names: Dict[str, sympy.Symbol]=None):
+    if symbol_names:
+        symbol_names_inversed = dict((v, k) for k, v in symbol_names.items())
+        ret = sympy.latex(expr, imaginary_unit="j", symbol_names=symbol_names_inversed)
+    else:
+        ret = sympy.latex(expr, imaginary_unit="j")
+    return ret
 
 def to_latex(dictionary: Dict[str, sympy.Expr], symbol_names: Dict[str, sympy.Symbol]=None) -> Dict[str, str]:
     """
@@ -172,17 +206,23 @@ def plot(func, var, start, stop, points, title="", x_label="x_axis", y_label="y_
     return fig
 
 
-def mag(func, decibel=False):
+def mag(func, decibel=False, do_evalf=True):
+    if do_evalf:
+        func = evalf(func, precision=20)
     if func != 0:
-        func = abs(func)
+        # some error is accrued during evalf, sympy for some reason returns a negligible Im from abs
+        func = sympy.re(abs(func))
         if decibel:
             func = 20*sympy.log(func, 10)
+            if do_evalf:
+                func = evalf(func, precision=20)
         return func
     else:
-        warning("Cannot plot 20*log(mag(func)), because func == 0!")
         return -infinity
 
-def arg(func, deg=False):
+def arg(func, deg=False, do_evalf=True):
+    if do_evalf:
+        func = evalf(func, precision=20)
     func = sympy.arg(func)
     if deg:
         func = sympy.deg(func)
@@ -211,8 +251,8 @@ def plot_phase(func, var, start, stop, points, title="Phase Plot"):
 
 def bode(func, start, stop, points=500):
     # Evaluate magnitude and phase
-    func_arg = arg(func, deg=True)  # phase in radians
-    func_mag = mag(func, decibel=True)  # magnitude in dB
+    func_arg = arg(func, deg=True, do_evalf=True)  # phase in radians
+    func_mag = mag(func, decibel=True, do_evalf=True)  # magnitude in dB
 
     # Frequency points (log scale)
     x = xpoints(start, stop, points, log=True)
@@ -222,7 +262,7 @@ def bode(func, start, stop, points=500):
     return x, y_mag, y_arg
 
 
-def plot_bode(func, start, stop, points=500, title="Bode Plot", param_list=None, param_symbol=None) -> "matplotlib.figure":
+def plot_bode(func, start, stop, points=500, title="Bode Plot", param_list=None, param_symbol=None, label_list=None) -> "matplotlib.figure":
     import matplotlib.pyplot as plt
     from numpy import array
 
@@ -250,6 +290,20 @@ def plot_bode(func, start, stop, points=500, title="Bode Plot", param_list=None,
             ax_mag.plot(arrx, arry_mag, label=f'mag_{i}')
             ax_phase.plot(arrx, arry_arg, label=f'arg_{i}', linestyle='--')
             i+=1
+    elif isinstance(func, list):
+        i = 0
+        if label_list is None:
+            labels = [x for x in range(len(func))]
+        else:
+            labels = label_list
+        for func_temp in func:
+            x, y_mag, y_arg = bode(func_temp, start, stop, points)
+            arrx = array(x)
+            arry_mag = array(y_mag)
+            arry_arg = array(y_arg)
+            ax_mag.plot(arrx, arry_mag, label=f'mag_{label_list[i]}')
+            ax_phase.plot(arrx, arry_arg, label=f'arg_{label_list[i]}', linestyle='--')
+            i += 1
     else:
         x, y_mag, y_arg = bode(func, start, stop, points)
         arrx = array(x)
@@ -259,10 +313,55 @@ def plot_bode(func, start, stop, points=500, title="Bode Plot", param_list=None,
         ax_phase.plot(arrx, arry_arg, label=f'arg', linestyle='--')
 
     # Title
-    ax_mag.legend(loc="upper left")
-    ax_phase.legend(loc="upper right")
+    ax_mag.legend(loc="lower left")
+    ax_phase.legend(loc="lower right")
     plt.title(title)
     fig.tight_layout()
     plt.show()
 
     return fig
+
+
+def plot_bode_num(values, control_freqs):
+    import matplotlib.pyplot as plt
+    from numpy import array
+    from sympy import sympify
+
+    # Create figure
+    fig, ax_mag = plt.subplots()
+    ax_mag.set_xscale("log")
+    ax_mag.set_xlabel("f (Hz)")
+    ax_mag.set_ylabel(r'$20 \log_{10} |H(j\omega)| \;(\mathrm{dB})$')
+    ax_mag.tick_params(axis='y')
+    ax_mag.grid(which='both', color='gray', alpha=0.7)
+
+    # Create second y-axis for phase
+    ax_phase = ax_mag.twinx()
+    ax_phase.set_ylabel(r'$\arg(H(j\omega)) \;(\mathrm{deg})$')
+    ax_phase.tick_params(axis='y')
+
+    x = control_freqs
+    y_mag = [mag(sympify(v), decibel=True) for v in values]
+    y_arg = [arg(sympify(v), deg=True) for v in values]
+    arrx = array(x)
+    arry_mag = array(y_mag)
+    arry_arg = array(y_arg)
+    ax_mag.plot(arrx, arry_mag, label=f'mag')
+    ax_phase.plot(arrx, arry_arg, label=f'arg', linestyle='--')
+
+    # Title
+    ax_mag.legend(loc="lower left")
+    ax_phase.legend(loc="lower right")
+    plt.title("Bode of bjt lab")
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_latex(latex_str, title=None):
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.axis('off')
+    if title:
+        plt.title(title)
+    plt.text(0.5, 0.5, f"${latex_str}$", ha='center', va='center', fontsize=9)
+    plt.show()
